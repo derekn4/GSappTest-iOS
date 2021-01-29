578eab2f26821714b67f25ae743f93f9d70b9ced
derekn4 <derekn4@uci.edu>
2021-01-28 20:34:35 -0800

    Initial Commit

---
+++ GSTest/GSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSTestApp.swift
+//  GSTest
+//
+//  Created by Derek Nguyen on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct GSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
